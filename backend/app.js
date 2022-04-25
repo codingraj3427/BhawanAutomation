bdd6c4d47602d5cef49e2ce2de17fccb70e944ce
coderaj2001 <biswajit05101963@gmail.com>
2022-04-25 13:58:53 +0530

    Some file i ahve added successfully

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,17 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-// const path = require("path");
-const dotenv=require("dotenv");
+const path = require("path");
 const errorMiddleware = require("./middleware/error");
 
 // Config  For the deployment
-// if (process.env.NODE_ENV !== "PRODUCTION") {
-//   require("dotenv").config({ path: "backend/config/config.env" });
-// }
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
 
 
-// For the local server
-dotenv.config({path:"backend/config/config.env"});
+// // For the local server
+// dotenv.config({path:"backend/config/config.env"});
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,11 +31,11 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 // Middleware for Errors
 app.use(errorMiddleware);
